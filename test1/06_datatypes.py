59709bd05e62d59e623aef458110b45fef0dc1f8
prashant <prashantkr.msh@gmail.com>
2023-02-08 11:14:55 +0530

    testing the repo

---
+++ test1/06_datatypes.py
@@ -0,0 +1,13 @@
+a=24
+print(a,type(a))
+b="prashant"
+print(b)
+print(type(b))
+c=True
+d=None
+print(c,type(c))
+print(d,type(d))
+print(str(a)+" "+b)
+e=complex(8,2)
+print(e,type(e))
+print(a+e)
